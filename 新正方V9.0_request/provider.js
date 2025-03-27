72af6a3e327b1595c1078d9c174f5bcffdd65b6a
kqakqakqa <58761476+kqakqakqa@users.noreply.github.com>
2025-03-27 16:46:09 +0800

    update

--- 新正方V9.0_request/provider.js
+++ 新正方V9.0_request/provider.js
@@ -69,9 +69,9 @@ async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom =
   }
 
   /* 获取课程数据请求参数 */
-  const defaultGnmkdm = undefined;
+  const defaultGnmkdm = "N2151";
   const useSu = false;
-  const useXsdm = false;
+  const useXsdm = true;
 
   let tryXnm, tryXqm, tryGnmkdm, trySu;
   try {
@@ -669,7 +669,136 @@ async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom =
    * @version 0.7.c6c0425
    */
   async function newLogFrame() {
-    // 粘贴到此处
+    // 删除已存在frame
+    document.querySelectorAll(".xiaoai-schedule-parser-log-base")?.forEach(e => e.remove());
+    // 设置背景
+    const baseElement = document.createElement("div");
+    document.body.append(baseElement);
+    baseElement.className = "xiaoai-schedule-parser-log-base";
+    baseElement.style.cssText = `
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(127, 127, 127, 0.28);
+        backdrop-filter: saturate(120%) blur(8px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: ${getMaxZIndex() + 1};
+      `;
+    function getMaxZIndex() {
+      return [...document.querySelectorAll("*")].reduce((r, e) => Math.max(r, +window.getComputedStyle(e).zIndex || 0), 0) || 1
+    }
+    // 设置卡片
+    const cardElement = document.createElement("iframe");
+    // cardElement.src = "about:blank";
+    baseElement.append(cardElement);
+    // await new Promise(r => {
+    //   const interval = setInterval(() => {
+    //     console.log(1)
+    //     if (cardElement.contentDocument || cardElement.contentWindow?.document) {
+    //       clearInterval(interval);
+    //       r();
+    //     }
+    //   }, 1);
+    // });
+    cardElement.style.cssText = `
+      display: block;
+      width: 100vh;
+      max-width: calc(80% - 40px);
+      max-height: calc(80% - 40px);
+      border: none;
+      border-radius: 10px;
+      padding: 20px;
+      background: rgba(255, 255, 255, 0.9);
+      box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+      box-sizing: content-box;
+      overflow: hidden;
+    `;
+    const iframeDocument = cardElement.contentDocument || cardElement.contentWindow?.document;
+    // 自动调整高度
+    new MutationObserver(() => {
+      cardElement.style.height = (iframeDocument.body.scrollHeight + 1) + "px"; // 有小数部分所以+1
+    }).observe(iframeDocument, { childList: true, subtree: true });
+    // 点击外围退出
+    baseElement.addEventListener("click", e => {
+      if (!cardElement.contains(e.target)) {
+        baseElement.remove();
+      }
+    });
+
+    function log(...msgs) {
+      for (const msg of msgs) {
+        console.log(msg);
+        if (typeof msg === "string") {
+          const e = document.createElement("span");
+          e.innerHTML = msg;
+          iframeDocument.body.append(...e.childNodes);
+        }
+        else {
+          iframeDocument.body.append(msg);
+        }
+      }
+      iframeDocument.body.scrollTo(0, iframeDocument.body.scrollHeight + 1);
+    }
+
+    baseElement.log = log;
+    baseElement.copyButton = copyButton;
+    baseElement.codeBlock = codeBlock;
+    baseElement.codeBlockShort = codeBlockShort;
+    baseElement.repoLink = repoLink;
+    return baseElement;
+
+
+    function copyButton(str) {
+      const copyButton = document.createElement("button");
+      copyButton.textContent = "点击复制";
+      copyButton.addEventListener("click", async e => {
+        const clipboard = navigator.clipboard ?? {
+          writeText: async (s) => new Promise(resolve => {
+            const input = document.createElement('input');
+            input.style.position = "absolute";
+            input.style.left = "-100vw";
+            input.value = s;
+            document.body.append(input);
+            input.select();
+            document.execCommand('copy');
+            input.remove();
+            resolve();
+          })
+        };
+        await clipboard.writeText(String(str));
+        e.target.textContent = "已复制";
+      });
+      return copyButton;
+    }
+
+    function codeBlock(str) {
+      const code = document.createElement("code");
+      code.style.cssText = `
+        background-color: #eee;
+        border-radius: 0.25em;
+        font-family: monospace;
+        padding: 0 0.5em;
+      `;
+      code.textContent = String(str);
+      return code;
+    }
+
+    function codeBlockShort(str) {
+      return codeBlock(String(str).replace(/^([\s\S]{10})[\s\S]*$/, "$1..."));
+    }
+
+    function repoLink() {
+      const e = document.createElement("span");
+      e.append("如果你需要，可以查看本适配项目源代码: ");
+      e.append(codeBlock("https://github.com/kqakqakqa/xiaoai-schedule-parser"));
+      e.append(copyButton("https://github.com/kqakqakqa/xiaoai-schedule-parser"));
+      return e;
+    }
+
   }
 
 
@@ -678,7 +807,153 @@ async function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom =
    * @version 0.12.7f27d73
    */
   function coursesPostProcessings() {
-    // 粘贴到此处
+    return {
+      check: check,
+      mergeConflictsAndDuplicates: mergeConflictsAndDuplicates,
+      mergeWeeks: mergeWeeks,
+      mergeTeachersOrPositions: mergeTeachersOrPositions,
+    };
+
+
+    // 有效性校验
+    function check(courses) { // todo: courses有效性校验(key是否存在, value是否格式匹配, number[]是否按顺序)
+      return courses;
+    }
+
+
+    // 合并重复&冲突课程
+    function mergeConflictsAndDuplicates(courses) {
+      const coursesCopy = JSON.parse(JSON.stringify(courses));
+      const courseMap = {};
+
+      for (const course of coursesCopy) {
+        for (const week of course.weeks) {
+          for (const section of course.sections) { // 拆成多门单周、单节课程
+            const key2 = JSON.stringify({
+              name: course.name,
+              position: course.position,
+              teacher: course.teacher,
+            });
+            const courseSplit1 = {
+              name: "",
+              position: "",
+              teacher: "",
+              weeks: [week], // 单周
+              day: course.day,
+              sections: [section], // 单节
+              key2s: [],
+            }
+            const key1 = JSON.stringify(courseSplit1);
+
+            if (!courseMap[key1]) courseMap[key1] = courseSplit1;
+            if (!courseMap[key1].key2s.includes(key2)) { // 舍弃重复课程, 合并冲突课程
+              courseMap[key1].name += (courseMap[key1].name === "" ? "" : ",") + course.name;
+              courseMap[key1].position += (courseMap[key1].position === "" ? "" : ",") + course.position;
+              courseMap[key1].teacher += (courseMap[key1].teacher === "" ? "" : ",") + course.teacher;
+              courseMap[key1].key2s.push(key2);
+            }
+          }
+        }
+      }
+
+      const courses2 = Object.values(courseMap).sort((a, b) => { // 排序
+        if (a.weeks[0] > b.weeks[0]) return (a.weeks[0] - b.weeks[0]);
+        if (a.day > b.day) return (a.day - b.day);
+        if (a.sections[0] > b.sections[0]) return (a.sections[0] - b.sections[0]);
+      });
+
+      const courses3 = [];
+      for (const course of courses2) { // 合并连续节次的课程
+        const key = JSON.stringify({ // name position teacher weeks day相同即为相同课程
+          name: course.name,
+          position: course.position,
+          teacher: course.teacher,
+          weeks: course.weeks,
+          day: course.day,
+        });
+        if (
+          courses3[courses3.length - 1]?.key === key &&
+          courses3[courses3.length - 1]?.sections?.includes(course.sections[0] - 1)
+        ) {
+          courses3[courses3.length - 1].sections.push(course.sections[0]);
+        } else {
+          courses3.push(course);
+          courses3[courses3.length - 1].key = key;
+        };
+      }
+
+      const courses4 = courses3.map(course => {
+        delete course.courseSplit2s;
+        delete course.key;
+        return course;
+      });
+
+      return courses4;
+    }
+
+
+    // 合并不同周数的相同课程
+    function mergeWeeks(courses) {
+      const coursesCopy = JSON.parse(JSON.stringify(courses));
+      const courseMap = {};
+
+      for (const course of coursesCopy) {
+        const course2 = {
+          name: course.name,
+          position: course.position,
+          teacher: course.teacher,
+          weeks: [],
+          day: course.day,
+          sections: course.sections,
+        };
+        const key = JSON.stringify(course2); // name position teacher day sections相同即为相同课程
+        if (!courseMap[key]) courseMap[key] = course2;
+        courseMap[key].weeks = Array.from(new Set(courseMap[key].weeks.concat(course.weeks))).sort((a, b) => a - b);// weeks合并, 去重, 排序
+      }
+
+      return Object.values(courseMap);
+    }
+
+
+    // 可选: 合并不同教师或教室的相同课程
+    function mergeTeachersOrPositions(courses, mergePositions = true, mergeTeachers = true) {
+      const coursesCopy = JSON.parse(JSON.stringify(courses));
+      const courseMap = {};
+
+      for (const course of coursesCopy) {
+        const course2 = {
+          name: course.name,
+          position: mergePositions || course.position,
+          teacher: mergeTeachers || course.teacher,
+          weeks: course.weeks,
+          day: course.day,
+          sections: course.sections,
+          positions: {},
+          teachers: {},
+        }
+        const key = JSON.stringify(course2); // name position(根据条件) teacher(根据条件) weeks day sections相同即为相同课程
+        if (!courseMap[key]) courseMap[key] = course2;
+        if (mergePositions) courseMap[key].positions[course.position] = Array.from(new Set(courseMap[key].positions[course.position].concat(course.weeks))).sort((a, b) => a - b); // position合并, position对应周数合并, 去重, 排序
+        if (mergeTeachers) courseMap[key].teachers[course.teacher] = Array.from(new Set(courseMap[key].teachers[course.teacher].concat(course.weeks))).sort((a, b) => a - b); // teacher合并, teacher对应周数合并, 去重, 排序
+
+      }
+
+      // 改回正确格式
+      const coursesNew = Object.values(courseMap).map(course => {
+        if (mergePositions) {
+          course.position = Object.entries(course.positions).map(([position, weeks]) => position + "(" + weeks.join(",") + "周)").join(" "); // eg. "教室A(1,2周) 教室B(3周)"
+          delete course.positions;
+        }
+        if (mergeTeachers) {
+          course.teacher = Object.entries(course.teachers).map(([teacher, weeks]) => teacher + "(" + weeks.join(",") + "周)").join(" "); // eg. "教师A(1,2周) 教师B(3周)"
+          delete course.teachers;
+        }
+        return course;
+      });
+
+      return coursesNew;
+    }
+
   }
 
 
